2ac900a4f91fdeae64878243aee936aa854e8110
sebastian <sebnarvaez19@gmail.com>
2024-02-27 22:07:36 +0000

    feat (create-client): use react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "beercss"
+import "material-dynamic-colors"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Root from "./routes/Root"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
