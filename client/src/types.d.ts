21f7620ca256bc80dbb17658f4f5d67b66497f15
sebastian <sebnarvaez19@gmail.com>
2024-02-27 21:15:23 +0000

    feat (create-client): types for User and Post

---
+++ client/src/types.d.ts
@@ -0,0 +1,17 @@
+export type User = {
+    id?: string,
+    name?: string,
+    username: string,
+    email: string,
+    bio?: string,
+    createdAt: string
+}
+
+export type Post = {
+    id?: string,
+    title: string,
+    body: string,
+    tags: string,
+    createdAt: string,
+    userId: string
+}
