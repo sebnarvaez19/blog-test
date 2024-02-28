8006fe21efb21e10888a3b8e4493440c46223a03
sebastian <sebnarvaez19@gmail.com>
2024-02-28 01:23:19 +0000

    fix (create-client): center elements in headbar

--- client/src/components/HeaderBar.tsx
+++ client/src/components/HeaderBar.tsx
@@ -6,13 +6,13 @@ export default function HeaderBar() {
     return (
         <header>
             <div className="grid">
-                <div className="s1 center-align middle align">
+                <div className="s1 center-align middle-align">
                     <HomeButton />
                 </div>
-                <div className="s10 center-align middle align">
+                <div className="s10 center-align middle-align">
                     <SearchBar />
                 </div>
-                <div className="s1 center-align middle align">
+                <div className="s1 center-align middle-align">
                     <MenuButton />
                 </div>
             </div>
