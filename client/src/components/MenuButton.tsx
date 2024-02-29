14e78b3a4a0a4c1a94c8b9c786365a179109e4af
sebastian <sebnarvaez19@gmail.com>
2024-02-29 15:08:14 +0000

    feat: add dropdown menu to menu button

--- client/src/components/MenuButton.tsx
+++ client/src/components/MenuButton.tsx
@@ -1,7 +1,14 @@
-export default function MenuButton() {
+const MenuButton: React.FC = () => {
     return (
-        <button className="circle transparent">
+        <button className="circle tertiary">
             <i>menu</i>
+            <menu className="left no-wrap">
+                <a>Login</a>
+                <a>Sign Up</a>
+                <a>Settings</a>
+            </menu>
         </button>
     )
 }
+
+export default MenuButton
