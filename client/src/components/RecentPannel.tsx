f702ffc8f78338e6e0f37263b767840f59075a96
sebastian <sebnarvaez19@gmail.com>
2024-02-29 00:43:19 +0000

    feat (recent-component): add recent activity component

--- client/src/components/RecentPannel.tsx
+++ client/src/components/RecentPannel.tsx
@@ -1,7 +1,24 @@
-export default function RecentPannel() {
+import { useFechtPosts } from "../hooks/fetchData";
+import { Post } from "../types";
+import MiniPost from "./MiniPost";
+
+const RecentPannel: React.FC = () => {    
+    const [posts,] = useFechtPosts() as [Post[], void]
+
     return (
-        <>
-            <h5>Recent...</h5>
-        </>
+        <article className="fill">
+            <div className="grid">
+                <div className="s12">
+                    <h6>Recent activity</h6>
+                </div>
+                {posts.slice(0, 5).map((post) => (
+                    <div className="s12 center-align middle-align">
+                        <MiniPost post={post}/>
+                    </div>
+                ))}
+            </div>
+        </article>
     )
 }
+
+export default RecentPannel
