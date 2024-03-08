4c19f108879164e34b39a5ccdd60c235c252814c
sebastian <sebnarvaez19@gmail.com>
2024-03-08 16:29:45 +0000

    fix (main): solve problem with enter key and new lines in other components

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,39 +1,20 @@
-import React, { FormEventHandler, useCallback, useContext, useEffect, useState } from "react"
+import React, { useCallback, useContext, useState } from "react"
 import SearchField from "./form_fields/SearchField"
 import { getPosts, searchPosts } from "../logic"
 import PostContext from "../contexts/PostContext"
+
 
 const SearchBar: React.FC = () => {
     const { setPosts } = useContext(PostContext)
     const [query, setQuery] = useState<string>("")
 
-    const submitHandler = useCallback(async () => {
-        let filteredPosts
-        if (query) {
-            filteredPosts = await searchPosts(query)
-        } else {
-            filteredPosts = await getPosts()
-        }
+    const submitHandler = useCallback(async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
 
+        const filteredPosts = query ? await searchPosts(query) : await getPosts()
         setPosts(filteredPosts)
     }, [query, setPosts])
 
-    useEffect(() => {
-        function keyDownHandler(e: FormEventHandler<HTMLFormElement>) {
-            if (e.key === "Enter") {
-                e.preventDefault()
-                submitHandler()
-            }
-        }
-
-        document.addEventListener("keydown", keyDownHandler)
-
-        return () => {
-            document.removeEventListener("keydown", keyDownHandler)
-        }
-    }, [submitHandler])
-
-    
     return (
         <form onSubmit={submitHandler}>
             <SearchField query={query} setQuery={setQuery} />
